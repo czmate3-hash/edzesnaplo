7c01af824ae761ddc06135a2466ecaaa866371a0
czmate3-hash <czmate3@gmail.com>
2026-08-28 11:37:24 +0200

    Bump PWA cache after final QA hardening

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'edzesnaplo-v6';
+const CACHE = 'edzesnaplo-v7';
 const CORE = ['./', './index.html', './styles.css', './app.js', './auth-route.js', './qa-fixes.js', './manifest.webmanifest', './icons/icon-192.svg', './icons/icon-512.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
